cd9ea8aaf0d98d059738029fd4b7e5c9e6fde004
marnionrails <37088724+marnionrails@users.noreply.github.com>
2021-09-12 11:50:10 -0400

    add all folders

---
+++ Bakery/Models/Bread.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+  namespace Bakery.Models
+  {
+    public class Bread
+    {
+
+      public int BreadNumber {get; set;} = 2;
+      public double BreadCost {get; set;} = 2.0;
+
+      public Bread(int breadNumber)
+      {
+        BreadNumber = breadNumber;
+        
+      }
+    }
+  }
---
+++ Bakery/Program.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+public class Program 
+{
+  public static void Main()
+  {
+    Console.WriteLine("Program");
+  }
+}
