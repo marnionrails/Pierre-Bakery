c7f9b3a8c21f35f1768bee2111870cf76e4ddd1f
marnionrails <37088724+marnionrails@users.noreply.github.com>
2021-09-12 23:33:44 -0400

    add Pastry.cs

---
+++ Bakery/Models/Pastry.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+  namespace Bakery.Models
+  {
+    public class Pastry
+    {
+
+      public int PastryNumber {get; set;} = 2;
+      public double PastryCost {get; set;} = 2.0;
+
+      public Pastry(int pastryNumber)
+      {
+        PastryNumber = pastryNumber;
+        
+      }
+    }
+  }
