cbb285159e9f1e77be49ae1c32d936a93b800454
marnionrails <37088724+marnionrails@users.noreply.github.com>
2021-09-12 22:51:08 -0400

    confirm we can create instance of Pastry

---
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using Bakery.Models;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class PastryTests
+  {
+    [TestMethod]
+    public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
+    {
+      Pastry newPastry = new Pastry(2);
+      Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+    }
+  }
+}
